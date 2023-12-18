7684695024e9c4cd6064674db07608936db060af
inv123 <nedyalko.tiholov@yahoo.com>
2023-12-18 16:00:53 +0200

    init winning combinations

--- src/initials.js
+++ src/initials.js
@@ -10,3 +10,46 @@ export const PLAYERS = [
         isActive: false
     }
 ]
+
+export const WINNING_COMBINATIONS=[
+    [
+        {row: 0, col: 0},
+        {row: 0, col: 1},
+        {row: 0, col: 2}
+    ],
+    [
+        {row: 1, col: 0},
+        {row: 1, col: 1},
+        {row: 1, col: 2}
+    ],
+    [
+        {row: 2, col: 0},
+        {row: 2, col: 1},
+        {row: 2, col: 2}
+    ],
+    [
+        {row: 0, col: 0},
+        {row: 1, col: 0},
+        {row: 2, col: 0}
+    ],
+    [
+        {row: 0, col: 1},
+        {row: 1, col: 1},
+        {row: 2, col: 1}
+    ],
+    [
+        {row: 0, col: 2},
+        {row: 1, col: 2},
+        {row: 2, col: 2}
+    ],
+    [
+        {row: 0, col: 0},
+        {row: 1, col: 1},
+        {row: 2, col: 2}
+    ],
+    [
+        {row: 0, col: 2},
+        {row: 1, col: 1},
+        {row: 2, col: 0}
+    ],
+]
